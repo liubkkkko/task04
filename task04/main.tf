0cc0c57e92b9675700e488cd87d60e8a3ebc9f0e
Liubomur Puliak <liubomurpuliak@gmail.com>
2025-03-16 23:19:52 +0200

    formated

--- task04/main.tf
+++ task04/main.tf
@@ -65,12 +65,12 @@ resource "azurerm_network_interface" "nic" {
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = var.vm_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = var.vm_sku
-  admin_username      = var.vm_username
-  admin_password      = var.vm_password
+  name                            = var.vm_name
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  size                            = var.vm_sku
+  admin_username                  = var.vm_username
+  admin_password                  = var.vm_password
   disable_password_authentication = false
 
   network_interface_ids = [azurerm_network_interface.nic.id]
